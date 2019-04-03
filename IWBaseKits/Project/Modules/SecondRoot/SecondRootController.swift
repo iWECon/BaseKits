7d4e496df30f97c5cd6622c7a0b994513d9306fa
SuTang <765768640@qq.com>
2019-04-03 13:40:52 +0800

    test-learn

--- IWBaseKits/Project/Modules/SecondRoot/SecondRootController.swift
+++ IWBaseKits/Project/Modules/SecondRoot/SecondRootController.swift
@@ -7,26 +7,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxDataSources
 
-class SecondRootController: IWViewController,UITableViewDelegate,UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return datas.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        var cell:UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: "mycell", for: indexPath)
-        if cell==nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        }
-        cell.textLabel?.text = "这个是第\(datas[indexPath.row].0)个" + datas[indexPath.row].2 + "盒子，编号:" + String(datas[indexPath.row].1)
-        return cell
-    }
-    
+class SecondRootController: IWViewController/*,UITableViewDelegate,UITableViewDataSource */{
     
     var tableView: UITableView!
-    var datas: [(String,Int,String)]!
+//    var datas: [(String,Int,String)]!   //元组
+    var datas : Observable<[(String,Int,String)]>?
     
 
 //    override func viewDidLoad() {
@@ -38,17 +26,51 @@ class SecondRootController: IWViewController,UITableViewDelegate,UITableViewData
 //        // Do any additional setup after loading the view.
 //    }
     
+    
     override func prepareUI() {
         super.prepareUI()
 
-        datas = [("1",8,"大"),("2",9,"中"),("3",3,"小")]
+        //赋值类型要和定义时保持一致
+//        datas =[("1",8,"大"),("2",9,"中"),("3",3,"小")]
+        
+        struct BoxItem {
+            let boxNum:String!
+            let boxModel:Int!
+            let boxType:String!
+        }
+        
+        var lists : Observable<[BoxItem]>
+        lists = Observable.just([
+//            BoxItem("1",8,"大")
+            ])
+
+        datas = Observable.just([
+            ("1",8,"大"),
+            ("2",9,"中"),
+            ("3",3,"小")
+        ])
 
         tableView = UITableView(frame: view.bounds, style: .grouped)
         tableView.backgroundColor = UIColor.white
-        tableView.delegate = self
-        tableView.dataSource = self
         view.addSubview(tableView)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mycell")
+        
+        datas?.bind(to:tableView.rx.items) {(tableView,row,item) in
+//            let cell = tableView.dequeueReusableCell(withIdentifier: "mycell") as! UITableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "mycell")
+            cell!.textLabel?.text = "第\(item.0)个" + item.2 + "盒子，编号:" + String(item.1)
+            return cell!
+            
+        }.disposed(by: rx.disposeBag)
+
+        tableView.rx.modelSelected((String,Int,String).self).subscribe(onNext: { (item) in
+            Console.log("第\(item.0)个" + item.2 + "盒子，编号:" + String(item.1))
+        }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: rx.disposeBag)
+        
+        tableView.rx.modelDeselected((String,Int,String).self).onNext { (item) in
+            Console.log("拜拜")
+        }.disposed(by: rx.disposeBag)
+        
     }
 
     /*
