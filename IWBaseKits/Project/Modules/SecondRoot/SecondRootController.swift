8b2e99f1e9999fdf19dcfd07b3ec5c6ccf66adc1
SuTang <765768640@qq.com>
2019-04-04 10:39:08 +0800

    fix merge.

--- IWBaseKits/Project/Modules/SecondRoot/SecondRootController.swift
+++ IWBaseKits/Project/Modules/SecondRoot/SecondRootController.swift
@@ -18,37 +18,6 @@ class SecondRootController: IWViewController{
     
     override func prepareUI() {
         super.prepareUI()
-<<<<<<< HEAD
-=======
-
-        struct BoxItem {
-            let boxNum:String!
-            let boxModel:Int!
-            let boxType:String!
-        }
-        
-        tableView = UITableView(frame: view.bounds, style: .grouped)
-        tableView.backgroundColor = UIColor.white
-        view.addSubview(tableView)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mycell")
-        
-        vm.datas?.bind(to:tableView.rx.items) {(tableView,row,item) in
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "mycell") as! UITableViewCell
-            let cell = tableView.dequeueReusableCell(withIdentifier: "mycell")
-            cell!.textLabel?.text = "第\(item.0)个" + item.2 + "盒子，编号:" + String(item.1)
-            return cell!
-            
-        }.disposed(by: rx.disposeBag)
-
-        tableView.rx.modelSelected((String,Int,String).self).subscribe(onNext: { (item) in
-            Console.log("第\(item.0)个" + item.2 + "盒子，编号:" + String(item.1))
-            
-            let vm = FourthRootViewModel.init(with: "123")
-            vm.present(true, completion: nil)
-            
-        }, onError: nil, onCompleted: nil, onDisposed: nil).disposed(by: rx.disposeBag)
->>>>>>> f8f7d1c55a19e14c2d58acaf982e4a1e64edfcb4
-        
     }
 
 }
