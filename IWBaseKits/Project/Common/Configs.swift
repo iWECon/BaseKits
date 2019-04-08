a3ecf05ca6b47fff03347e548c534ad115c471b8
iWw <i.1214@yahoo.com>
2019-04-08 08:54:17 +0800

    加入Configs, 作为全局配置的一个文件

---
+++ IWBaseKits/Project/Common/Configs.swift
@@ -0,0 +1,22 @@
+//
+//  Configs.swift
+//  IWBaseKits
+//
+//  Created by 未来 on 2019/4/7.
+//  Copyright © 2019 iWECon. All rights reserved.
+//
+
+import UIKit
+
+public struct Configs {
+    private init() { }
+    
+    static let cornerRadisu = 10.cgFloat
+    
+    static let backgroundColor = UIColor.groupTableViewBackground
+    
+    static let mainColor = UIColor.hex(0xff512b)
+    static let subColor = UIColor.hex(0xcccccc)
+    
+    
+}
